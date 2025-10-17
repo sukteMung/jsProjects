f852cc9256a9bf8f0b8f899e4d1306603396027b
sukteMung <tur58332@temple.edu>
2025-10-17 05:57:31 -0400

    Update 10/17

---
+++ P02_Secret/script.js
@@ -0,0 +1,3 @@
+const memory_box = document.querySelector('.memory_container');
+const in_game_box = document.querySelector('.in_game_container');
+const quotes_box = document.querySelector('.quotes_container');
